ffd24402a518acf0503850162ce88f6c0a407ab9
WJWH <wjw.hillen@gmail.com>
2013-09-26 18:05:09 +0200

    Changes to cabal file to improve description and homepage, added program describing comments to gpiotest.hs

--- test/gpiotest.hs
+++ test/gpiotest.hs
@@ -4,6 +4,9 @@ import Control.Monad
 
 --compile with ghc --make gpiotest.hs ~/bcm2835-1.25/src/bcm2835.o to prevent "reference not found" errors
 
+--This program switches pin 11 on and off, with roughly a second between state transitions. Attach a LED in series
+--with a resistor between pin 11 and GND to see the LED blinking.
+
 main = withGPIO $ do
     setPinFunction Pin11 Output
     forever $ do
