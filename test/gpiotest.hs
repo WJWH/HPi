e50cdb2a23cd226db7aa552a8db7407ab243e69e
WJWH <wjw.hillen@gmail.com>
2013-11-10 13:23:42 +0100

    Added compilation and sudo explanation to gpiotest.hs

--- test/gpiotest.hs
+++ test/gpiotest.hs
@@ -2,7 +2,11 @@ import System.RaspberryPi.GPIO
 import Control.Concurrent
 import Control.Monad
 
---compile with ghc --make gpiotest.hs ~/bcm2835-1.25/src/bcm2835.o to prevent "reference not found" errors
+--When compiling this, make sure to include the path to the bcm2835 library to the compilerto prevent 
+--"reference not found" errors, ie:
+--ghc --make gpiotest.hs ~/bcm2835-1.25/src/bcm2835.o 
+--Obviouly, this assumes you installed the bcm2835 library in "~/bcm2835-1.25". Due to the library acessing
+-- /dev/mem, the compiled program should be run with sudo.
 
 --This program switches pin 11 on and off, with roughly a second between state transitions. Attach a LED in series
 --with a resistor between pin 11 and GND to see the LED blinking.
