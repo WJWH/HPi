f48874ba93de2673f65ea898b7d5407644256a74
WJWH <wjw.hillen@gmail.com>
2013-11-27 20:01:12 +0100

    Bugfix in exception handling code.

--- System/RaspberryPi/GPIO.hs
+++ System/RaspberryPi/GPIO.hs
@@ -121,7 +121,7 @@ withI2C f = bracket_    initI2C
 
 --
 
-actOnResult rr buf = case readresult of
+actOnResult rr buf = case rr of
     0x01 -> throwIO $ IOError Nothing IllegalOperation "I2C: " "Received an unexpected NACK." Nothing Nothing
     0x02 -> throwIO $ IOError Nothing IllegalOperation "I2C: " "Received Clock Stretch Timeout." Nothing Nothing 
     0x04 -> throwIO $ IOError Nothing IllegalOperation "I2C: " "Not all data was read." Nothing Nothing
@@ -201,7 +201,7 @@ setI2cClockDivider a = c_setClockDividerI2C $ fromIntegral a
 setI2cBaudRate :: Word32 -> IO ()
 setI2cBaudRate a = c_setBaudRateI2C $ fromIntegral a
 
--- |Writes the data in the 'ByteString' to the specified adress. Throws an 
+-- |Writes the data in the 'ByteString' to the specified adress. Throws an IOException if an error occurs.
 writeI2C :: Address -> BS.ByteString -> IO ()	--writes a bytestring to the specified address
 writeI2C address by = BS.useAsCString by $ \bs -> do
     setI2cAddress address
@@ -212,7 +212,7 @@ writeI2C address by = BS.useAsCString by $ \bs -> do
         0x04 -> throwIO $ IOError Nothing IllegalOperation "I2C: " "Not all data was read." Nothing Nothing  return (Left "Not all data was read.")
         0x00 -> return ()
 
--- |Reads num bytes from the specified address. THrows an 
+-- |Reads num bytes from the specified address. Throws an IOException if an error occurs.
 readI2C :: Address -> Int -> IO () --reads num bytes from the specified address
 readI2C address num = allocaBytes (num+1) $ \buf -> do --is the +1 necessary??
     setI2cAddress address
@@ -220,7 +220,7 @@ readI2C address num = allocaBytes (num+1) $ \buf -> do --is the +1 necessary??
     actOnResult readresult buf
 
 -- |Writes a 'ByteString' containing a register address to the specified address, then reads num bytes from
--- it, using the \"repeated start\" I2C method. Throws an I
+-- it, using the \"repeated start\" I2C method. Throws an IOException if an error occurs.
 writeReadI2C :: Address -> BS.ByteString -> Int -> IO ()
 writeReadI2C address by num = BS.useAsCString by $ \bs -> do --marshall the register-containing bytestring
     allocaBytes (num+1) $ \buf -> do	--allocate a buffer for the response
