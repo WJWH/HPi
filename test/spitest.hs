a935c05dcdd71f05ec75dbea1024874e9624b829
Wander Hillen <wjw.hillen@gmail.com>
2015-09-25 17:15:04 +0200

    Add a Set Write Enable Latch command to the spitest.hs example and clean up the documentation a bit.

--- test/spitest.hs
+++ test/spitest.hs
@@ -15,16 +15,17 @@ import Data.Word
 --The chip select pin of the FRAM should be on CS1
 
 main = withGPIO . withSPI $ do
-    chipSelectSPI CS1   --set the Chip select pin to the CS1 pin
-    setChipSelectPolaritySPI CS1 False --The FRAM chip needs CS to be puled low for transactions
+    chipSelectSPI CS0   --set the Chip select pin to the CS0 pin
+    setChipSelectPolaritySPI CS0 False --The FRAM chip needs CS to be pulled low for transactions
     setDataModeSPI (False,False) --The FRAM chip can handle either datamode 0,0 or 1,1
     mapM_ writeReadAndPrint [1..10]
     
 writeReadAndPrint :: Word8 -> IO ()
 writeReadAndPrint x = do
+    transferManySPI [6,0] --set the Write Enable Latch, this resets after each WRITE command
     --for a deeper explanation of the values used below, see the datasheet of the FRAM chip
-    --the "2" means write, the two zeroes mean memory adress zero and the final value is the value actually written
+    --the "2" means write, the next two values are the adress to use and the final value is the value actually written
     transferManySPI [2,0,0,x] 
-    --the "3" means read, the next two zeroes mean memory adress zero and the last one is ignored by the chip but is necessary 
+    --the "3" means read, the next two values are the adress to use and the last one is ignored by the chip but is necessary 
     --to keep the transaction open for the read
     transferManySPI [3,0,0,0] >>= (print . last)  -- should print x
